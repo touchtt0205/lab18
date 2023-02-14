8d92e4c1b25278cf5f906b84ba707df4dd4af677
Your Name <you@example.com>
2023-02-14 16:28:25 +0700

    FULL

--- lab18_3.cpp
+++ lab18_3.cpp
@@ -22,3 +22,23 @@ int main(){
 }
 
 //Write Definition of randomVector() showVector() and dotProduct() here
+vector<int> randomVector(unsigned int N){	
+    vector<int> v;
+    for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10);
+    return v;
+}
+
+void showVector(vector<int> z){
+	cout << "[";
+	for(unsigned int i = 0; i<z.size(); i++){
+		cout << z[i];
+		if(i == z.size()-1 ) cout << "]";
+		else cout << " ";
+	}
+}
+
+int dotProduct(vector<int> x,vector<int> y){
+	int sum = 0;
+	for(unsigned int i = 0; i<x.size(); i++) sum += x[i]*y[i] ;	
+	return sum;
+}
